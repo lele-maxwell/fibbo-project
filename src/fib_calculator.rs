2af0c5edb58128689d0f6e7517ddee285069178b
lele-maxwell <leleivanlele22@gmail.com>
2025-03-04 12:32:19 +0100

    Update fib_calculator.rs

--- src/fib_calculator.rs
+++ src/fib_calculator.rs
@@ -1,14 +1,8 @@
-use num_bigint::{BigInt, ToBigInt};
-
-//#[derive(Debug)]
-//pub struct Fibbonacci;
-
-//impl Fibbonacci {
     pub fn fibbo(num: i32) -> i32 {
-      let mut previous_number: i32 = 1;//.to_bigint().unwrap();
-      let mut current_number: i32 = 0;//.to_bigint().unwrap();
-      
-      let mut  i = 0;//.to_bigint().unwrap();
+      let mut previous_number: i32 = 1;
+      let mut current_number: i32 = 0;
+        
+      let mut  i = 0;
       while i < num {
           let previous_previous_number = previous_number;
           previous_number = current_number + &previous_previous_number;
